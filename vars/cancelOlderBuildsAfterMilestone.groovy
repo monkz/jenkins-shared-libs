9c3c960cde7e613205cf654ffd03c9844c4d939e
MonkZ <git@monkz.de>
2017-10-11 00:09:03 +0200

    initial commit based on https://github.com/cloudbees/jenkins-scripts/blob/master/cancel-builds-same-job.groovy

---
+++ vars/cancelOlderBuildsAfterMilestone.groovy
@@ -0,0 +1,17 @@
+/*
+Author: Isaac S Cohen
+This script works with workflow to cancel other running builds for the same job
+Use case: many build may go to QA, but only the build that is accepted is needed,
+the other builds in the workflow should be aborted
+*/
+def call(){
+	def jobname = env.JOB_NAME
+	def buildnum = env.BUILD_NUMBER.toInteger()
+	
+	def job = Jenkins.instance.getItemByFullName(jobname)
+	for (build in job.builds) {
+		if (!build.isBuilding()) { continue; }
+		if (build.getNumber().toInteger() >= buildnum) { continue; println "equals or newer" }
+		build.doStop();
+	}
+}
